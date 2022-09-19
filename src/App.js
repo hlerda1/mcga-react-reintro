b0fedf444b96b65070faa9527ef49d9604df2005
Horacio Lerda <horacio.lerda@gmail.com>
2022-09-19 19:01:51 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import { useState } from 'react';
+import './App.css';
+import Form from './components/Form';
+import TodoItem from './components/TodoItem';
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+        title: 'List of To Dos',
+        id: '01',
+        
+    },
+  ]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo])
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Form addTodo={addTodo} />
+        {todos.map((todo) => (
+            <TodoItem todo={todo} key={todo.id}/>
+        ))}
+        {!todos.length && <p>No todos</p>}
     </div>
   );
 }
